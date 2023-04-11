b02a1506657b827a08d1f7077bdec95f5027343b
2017250297-choi <97ckdtn@gmail.com>
2023-04-11 22:31:44 +0900

    add docstring and hint

--- User_app/views.py
+++ User_app/views.py
@@ -2,11 +2,16 @@ from .models import Profile
 from django.shortcuts import render, redirect
 from django.contrib.auth import get_user_model
 from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse, HttpResponseRedirect
 
 # Create your views here.
 
 
-def profile_view(request, id):
+def profile_view(request, id: int) -> HttpResponse | HttpResponseRedirect:
+    '''
+    모든 사용자가 프로필 페이지를 조회할 수 있습니다. 프로필과 프로필 소유자의 글 목록이 주어집니다.
+    오직 프로필 소유자 일때만 프로필의 수정을 할 수 있습니다.
+    '''
     # 프로필 가져오기
     opened_profile = Profile.objects.get(id=id)
     if request.method == 'GET':
