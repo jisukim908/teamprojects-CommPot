b3a22340483f2047d35e4a71098b546de5322cb3
2017250297-choi <97ckdtn@gmail.com>
2023-04-11 17:53:40 +0900

    create profile_view function

--- User_app/views.py
+++ User_app/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from .models import Profile
+from django.shortcuts import render, redirect
+from django.contrib.auth import get_user_model
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+
+
+def profile_view(request, id):
+    # 프로필 가져오기
+    opened_profile = Profile.objects.get(id=id)
+    if request.method == 'GET':
+        # 역참조로 지정된 사용자의 글만 가져오기
+        posts = opened_profile.username.Posting_set.order_by('-created_at')
+        return render('', {'profile': opened_profile, 'posts': posts})
+    if request.method == 'PUT':
+        if request.user != opened_profile.username:
+            return redirect('/')
+        locate = request.PUT.get('locate', '')
+        description = request.PUT.get('description', '')
+        opened_profile.locate = locate
+        opened_profile.description = description
+        opened_profile.save()
+        return redirect('/api/proflie/'+id)
