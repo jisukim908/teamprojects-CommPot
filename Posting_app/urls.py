e41d8536ce006ccb47bfb8c9244d150339630bc5
jisu <onpaso53@gmail.com>
2023-04-12 12:54:30 +0900

    Write Posting_app

---
+++ Posting_app/urls.py
@@ -0,0 +1,10 @@
+#api/posts/{id} urls
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('api/posts/<int:id>',views.posting_detail,name = 'detail-posting'),
+    path('api/posts/delete/<int:id>',views.delete_posting,name = 'delete-posting'),
+    path('api/posts/comment/<int:id>',views.write_comment,name = 'write-comment'),
+    path('api/posts/comment/delete/<int:id>',views.delete_comment,name = 'delete-comment'),
+]
