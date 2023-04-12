cd8535cf8db3f18fc8a3bba3e509e98804942659
juyakim <s9t7a9r2@naver.com>
2023-04-12 12:55:23 +0900

    로그인기능

--- User_app/admin.py
+++ User_app/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import UserModel
 
-# Register your models here.
+# 나의 UserModel을 Admin에 추가
+admin.site.register(UserModel)
--- User_app/views.py
+++ User_app/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import UserModel
 
-# Create your views here.
+
+# User_app/views.py
+def sign_up_view(request):
+    if request.method == "GET":  # GET 메서드로 요청이 들어 올 경우
+        return render(request, "user/signup.html")
+    elif request.method == "POST":  # POST 메서드로 요청이 들어 올 경우
+        username = request.POST.get("username", None)
+        password = request.POST.get("password", None)
+        password2 = request.POST.get("password2", None)
+        bio = request.POST.get("bio", None)
+
+        if password != password2:
+            return render(request, "user/signup.html")
+        else:
+            new_user = UserModel()
+            new_user.username = username
+            new_user.password = password
+            new_user.bio = bio
+            new_user.save()
+        return redirect("/sign-in")
+
+
+def sign_in_view(request):
+    return render(request, "user/signin.html")
