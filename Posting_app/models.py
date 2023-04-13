d702ce58243cc22912a4882de36a5a00e3cce395
2017250297-choi <97ckdtn@gmail.com>
2023-04-13 20:38:41 +0900

    update posting model,view,base.html for category

--- Posting_app/models.py
+++ Posting_app/models.py
@@ -11,10 +11,17 @@ from User_app.models import UserModel
 class Posting(models.Model):
     class Meta:
         db_table = "posting"
+    CATEGORIES = (
+        ('recipe', 'Recipes'),
+        ('recommend', 'Recommendation'),
+        ('chat', 'Chat'),
+    )
 
     author = models.ForeignKey(UserModel, on_delete=models.CASCADE)
-    title = models.CharField(max_length=50,verbose_name='글 제목')
-    content = models.CharField(max_length=10000000,verbose_name='글 내용')
+    title = models.CharField(max_length=50, verbose_name='글 제목')
+    content = models.CharField(max_length=10000000, verbose_name='글 내용')
+    category = models.CharField(
+        choices=CATEGORIES, max_length=9, verbose_name='카테고리')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
--- Posting_app/views.py
+++ Posting_app/views.py
@@ -5,11 +5,16 @@ from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 # Create your views here.
 
-    
-    
+
 def post_view(request):
     if request.method == 'GET':
-        all_post = Posting.objects.all().order_by('-created_at')
+        category = request.GET.get('category', '')
+        all_post = []
+        if category:
+            all_post = Posting.objects.filter(
+                category=category).order_by('-created_at')
+        else:
+            all_post = Posting.objects.all().order_by('-created_at')
         return render(request, 'posting/post.html', {'posts': all_post})
     elif request.method == 'POST':
         user = request.user.is_authenticated
@@ -22,7 +27,8 @@ def post_view(request):
         my_post.content = request.POST.get('my-content', '')
         my_post.save()
         return redirect('/api/posts/')
-        
+
+
 @login_required
 def delete_posting_view(request, id):
     my_post = Posting.objects.get(id=id)
@@ -30,31 +36,31 @@ def delete_posting_view(request, id):
         my_post.delete()
     return redirect('/api/posts')
     # return HttpResponse('글 삭제 완료')
-    
+
 
 def posting_detail_view(request, id):
     # 수정하기
     if request.method == 'POST':
         my_posting = Posting.objects.get(id=id)
         if request.user != my_posting.author:
-             return redirect('/api/posts/'+str(id))
+            return redirect('/api/posts/'+str(id))
         title = request.POST.get("title", "")
         content = request.POST.get("content", "")
         my_posting.title = title
         my_posting.content = content
         my_posting.save()
-        #return redirect('/api/posts/'+str(id))
+        # return redirect('/api/posts/'+str(id))
         return HttpResponse('수정완료')
     elif request.method == 'GET':
         my_posting = Posting.objects.get(id=id)
-        posting_comment = PostingComment.objects.filter(posting_id = id)
-        #return render(request,'posting/posting_detail.html',{'posting':my_posting,'comment':posting_comment})
+        posting_comment = PostingComment.objects.filter(posting_id=id)
+        # return render(request,'posting/posting_detail.html',{'posting':my_posting,'comment':posting_comment})
         return HttpResponse('업로드 완료')
 
 
-def write_comment_view(request,id):
+def write_comment_view(request, id):
     if request.method == 'POST':
-        comment = request.POST.get("comment","")
+        comment = request.POST.get("comment", "")
         current_posting = Posting.objects.get(id=id)
         user = request.user.is_authenticated
         if not user:
@@ -62,15 +68,15 @@ def write_comment_view(request,id):
         PC = PostingComment()
         PC.comment = comment
         PC.author = request.user
-        PC.posting =  current_posting
+        PC.posting = current_posting
         PC.save()
 
-        #return redirect('/api/posts'+str(id))
+        # return redirect('/api/posts'+str(id))
         return HttpResponse('댓글 저장 완료')
 
 
 @login_required
-def delete_comment_view(request,id):
+def delete_comment_view(request, id):
     comment = PostingComment.objects.get(id=id)
     current_posting = comment.posting.id
     if comment.author == request.user:
@@ -78,4 +84,3 @@ def delete_comment_view(request,id):
 
     # return redirect('/api/posts'+str(current_posting))
     return HttpResponse('댓글 삭제 완료')
-
