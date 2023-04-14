07f06eda1c924a1eb840ca271c16feaa3d24ff5a
2017250297-choi <97ckdtn@gmail.com>
2023-04-14 09:43:49 +0900

    change posting content to textfield

--- Posting_app/models.py
+++ Posting_app/models.py
@@ -19,7 +19,7 @@ class Posting(models.Model):
 
     author = models.ForeignKey(UserModel, on_delete=models.CASCADE)
     title = models.CharField(max_length=50, verbose_name='글 제목')
-    content = models.CharField(max_length=10000000, verbose_name='글 내용')
+    content = models.TextField(verbose_name='글 내용')
     category = models.CharField(
         choices=CATEGORIES, max_length=9, verbose_name='카테고리')
     created_at = models.DateTimeField(auto_now_add=True)
