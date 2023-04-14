6a2cbfad00366ca4cf3371b535f8346bb9215dfa
gangyuji <gangyoyul9@gmail.com>
2023-04-14 13:33:17 +0900

    post write and detail edited

--- Posting_app/views.py
+++ Posting_app/views.py
@@ -50,22 +50,36 @@ def posting_detail_view(request, id):
     posting_comment = PostingComment.objects.filter(posting_id = id)
     return render(request,'posting/post_detail.html',{'posting':my_posting,'comment':posting_comment})
    
-
+@login_required
 def posting_edit_view(request, id):
-    my_posting = Posting.objects.get(id=id)
-    if request.method == 'POST':
-        my_posting = Posting.objects.get(id=id)
-        if request.user != my_posting.author:
-            return redirect('/api/posts/'+str(id))
-
-        title = request.POST.get("title", "")
-        content = request.POST.get("content", "")
-        my_posting.title = title
-        my_posting.content = content
-        my_posting.save()
-        return redirect('/api/posts/'+str(id))
+    # 1. id가 0 인지 확인
+    if id==0:
+        my_posting=Posting()
+        my_posting.title=""
+        my_posting.content=""
+        my_posting.category="recipe"
+        my_posting.author=request.user
+    
+    # 2. id가 0 이면, 새로운 포스팅 모델 객체를 만들면 됩니다.
+    # 3. 0이 아니라면, 바로 여기아래 있는 코드한줄이 실행되면 됩니다.
     else:
         my_posting = Posting.objects.get(id=id)
+    # 4. 작성자와 동일한 사용자인지 체크하고 아니면 되돌려보내기
+    if request.user != my_posting.author:
+        return redirect('/api/posts/'+str(id)) if id else redirect('/api/posts')
+    if request.method == 'POST':
+        title = request.POST.get("title", "")
+        content = request.POST.get("content", "")
+        category=request.POST.get("category", "recipe")
+        my_posting.title = title
+        my_posting.content = content
+        # title, content 내용 없으면 다시 랜더링
+        if (title.strip() == "")or(content.strip()==""):
+            return render(request, 'posting/post_edit.html', {'posting':my_posting,'error':"please write title and content!"})
+        my_posting.category=category
+        my_posting.save()
+        return redirect('/api/posts/'+str(id)) if id else redirect('/api/posts')
+    else:
         return render(request, 'posting/post_edit.html', {'posting':my_posting})
 
 
